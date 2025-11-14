68894f263d20859fe72b68f3a99df44456647fcf
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2025-11-14 11:05:07 +0100

    Added Javascript

---
+++ src/script.js
@@ -0,0 +1,34 @@
+function updateTime() {
+
+//Los Angeles time
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz("America/Los_Angeles");
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+// Lion time
+
+let lionElement = document.querySelector("#lion");
+let lionDateElement = lionElement.querySelector(".date");
+let lionTimeElement = lionElement.querySelector(".time");
+let lionTime = moment().tz("Frnace/Lion");
+lionDateElement.innerHTML = lionTime.format("MMMM Do YYYY");
+lionTimeElement.innerHTML = lionTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+// Warsaw time
+let warsawElement = document.querySelector("#warsaw");
+let warsawDateElement = warsawElement.querySelector(".date");
+let warsawTimeElement = warsawElement.querySelector(".time");
+let warsawTime = moment().tz("Frnace/Lion");
+warsawDateElement.innerHTML = warsawTime.format("MMMM Do YYYY");
+    warsawTimeElement.innerHTML = warsawTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
